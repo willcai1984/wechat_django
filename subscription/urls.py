4ef8618eb1b1090f3b4235e4f8e14724f86eb39d
Will <willcai1984@gmail.com>
2018-11-25 23:38:03 +0800

    init version

--- subscription/urls.py
+++ subscription/urls.py
@@ -6,6 +6,6 @@ app_name = 'subscription'
 urlpatterns = [
     # path('subscription/', include('subscription.urls')),
     path('weixin/', views.weixin_check, name='weixin_check'),
-    path('auth', views.AuthView.as_view()),
+    path('auth/', views.AuthView.as_view()),
     path('index', views.GetInfoView.as_view()),
 ]
