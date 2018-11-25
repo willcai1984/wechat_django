6f19561d888d2be9838fd158192951cd0965e402
Will <willcai1984@gmail.com>
2018-11-25 22:21:26 +0800

    init version

--- subscription/views.py
+++ subscription/views.py
@@ -9,19 +9,19 @@ from django.http import HttpResponse
 @csrf_exempt
 def weixin_check(request):
     if request.method == 'GET':
-        signature = request.GET.get('signature')
-        timestamp = request.GET.get('timestamp')
-        nonce = request.GET.get('nonce')
-        echostr = request.GET.get('echostr')
-        token = 'leartd'
-
+        signature = request.GET.get('signature', None)
+        timestamp = request.GET.get('timestamp', None)
+        nonce = request.GET.get('nonce', None)
+        echostr = request.GET.get('echostr', None)
+        token = 'yiqian'
         hashlist = [token, timestamp, nonce]
         hashlist.sort()
         print('[token, timestamp, nonce]', hashlist)
-        hashstr = ''.join([s for s in hashlist]).encode('utf-8')
+        hashstr = ''.join(hashlist).encode('utf-8')
         print('hashstr before sha1', hashstr)
         hashstr = hashlib.sha1(hashstr).hexdigest()
-        print('hashstr sha1', hashstr)
+        print('hashstr after sha1', hashstr)
+        print('signature is ', signature)
         if hashstr == signature:
             return HttpResponse(echostr)
         else:
