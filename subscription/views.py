955691a8a53ec211501de248caf8b1008210f9b4
Will <willcai1984@gmail.com>
2018-11-25 23:15:32 +0800

    init version

--- subscription/views.py
+++ subscription/views.py
@@ -1,8 +1,13 @@
 # -*- coding: utf-8 -*-
-
 import hashlib
 from django.views.decorators.csrf import csrf_exempt
 from django.http import HttpResponse
+from django.views.generic import View
+from django.http import HttpResponseRedirect
+from django.http import HttpResponse, HttpResponseServerError
+from django.shortcuts import render, redirect
+from .util import WechatLogin
+from django.http import JsonResponse
 
 
 # django默认开启csrf防护，这里使用@csrf_exempt去掉防护
@@ -28,3 +33,47 @@ def weixin_check(request):
             return HttpResponse('error')
     else:
         return HttpResponse('...')
+
+
+class WechatViewSet(View):
+    wechat_api = WechatLogin()
+
+
+class AuthView(WechatViewSet):
+    def get(self, request):
+        url = self.wechat_api.get_code_url()
+        return redirect(url)
+
+
+class GetInfoView(WechatViewSet):
+    def get(self, request):
+        if 'code' in request.GET:
+            code = request.GET['code']
+            token, openid = self.wechat_api.get_access_token(code)
+            if token is None or openid is None:
+                return HttpResponseServerError('get code error')
+            user_info, error = self.wechat_api.get_user_info(token, openid)
+            if error:
+                return HttpResponseServerError('get access_token error')
+            user_data = {
+                'nickname': user_info['nickname'],
+                'sex': user_info['sex'],
+                'province': user_info['province'].encode('iso8859-1').decode('utf-8'),
+                'city': user_info['city'].encode('iso8859-1').decode('utf-8'),
+                'country': user_info['country'].encode('iso8859-1').decode('utf-8'),
+                'avatar': user_info['headimgurl'],
+                'openid': user_info['openid']
+            }
+            return JsonResponse(user_data)
+
+            # user = BeautyUsers.objects.filter(is_effective=True).filter(wechat=user_data['openid'])
+            # if user.count() == 0:
+            #     user = BeautyUsers.objects.create(username=user_data['nickname'],
+            #                                       wechat_avatar=user_data['avatar'],
+            #                                       wechat=user_data['openid'],
+            #                                       password='')
+            #     login(request, user)
+            # else:
+            #     login(request, user.first())
+            # # 授权登录成功，进入主页
+            # return home(request)
