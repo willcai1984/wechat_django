fabe4d728667c2dd199699f0e7a86a72a8bc66ef
caiwei <caiwei@qjdchina.com>
2018-12-12 09:15:30 +0800

    Add sql default value

---
+++ mybook/models.py
@@ -0,0 +1,11 @@
+from django.db import models
+
+# Create your models here.
+from django.db import models
+import django.utils.timezone as timezone
+
+
+# Create your models here.
+class Book(models.Model):
+    book_name = models.CharField('书名', max_length=64, default='')
+    add_time = models.DateTimeField('创建日期', default=timezone.now)
