77000c4ed2513192e39634c254c0447958f7ea69
caiwei <caiwei@qjdchina.com>
2018-12-14 20:55:58 +0800

    Add sql default value

--- subscription/views.py
+++ subscription/views.py
@@ -198,22 +198,11 @@ class AccountPay(View):
         :param kwargs:
         :return:
         """
-        # getInfo = request.GET.get('getInfo', None)
-        # open_id = request.COOKIES.get('open_id', '')
         open_id = request.GET['open_id']
         if not open_id:
-            # if getInfo != 'yes':
-            #     # 构造一个url，携带一个重定向的路由参数，
-            #     # 然后访问微信的一个url,微信会回调你设置的重定向路由，并携带code参数
-            #     return HttpResponseRedirect(get_redirect_url())
-            # elif getInfo == 'yes':
-            # 我设置的重定向路由还是回到这个函数中，其中设置了一个getInfo=yes的参数
-            # 获取用户的openid
             open_id = get_openid(request.GET.get('code'), request.GET.get('state', ''))
             if not open_id:
                 return HttpResponse('获取用户openid失败')
-        # else:
-        #     return HttpResponse('获取机器编码失败')
         response = render(request, '3account_pay.html', context={'params': get_jsapi_params(open_id)})
         response.set_cookie('openid', open_id, expires=60 * 60 * 24 * 30)
         return response
